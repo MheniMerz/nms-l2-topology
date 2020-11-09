ce7e5dbb4f5e447db0bfb127be19c5f67f12792c
mheniMerz <mheni.merzouki@nist.gov>
2020-11-09 14:05:49 +0000

    :rotating_light: add exception handeling for ssh connection attempt

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
 import netmiko
+from netmiko.ssh_exception import  NetMikoTimeoutException
+from paramiko.ssh_exception import SSHException 
+from netmiko.ssh_exception import  AuthenticationException
 import configparser
 import json
 from types import SimpleNamespace
 import concurrent.futures
 from ntc_templates.parse import parse_output
+
 from models.node import Node
 import models.ltp
 from models.ltp import Ltp
 from models.link import Link
 
 def run_cmd(ip, dev_type, username, password, command):
-    # create connection
-    connection = netmiko.ConnectHandler(
-            ip=ip,
-            device_type=dev_type,
-            username=username,
-            password=password,
-    )
-    # run the first command to get the ltps on the current node
-    result = connection.send_command(command)
+    try:
+        # create connection
+        connection = netmiko.ConnectHandler(
+                ip=ip,
+                device_type=dev_type,
+                username=username,
+                password=password,
+        )
+        # run the first command to get the ltps on the current node
+        result = connection.send_command(command)
+    except (AuthenticationException):
+        print("***** AUTHENTICATION ERROR when connecting to "+ip+" *****")
+        return
+    except (SSHException):
+        print("***** SSH ERROR when connecting to "+ip+" *****")  
+        return
+    except (NetMikoTimeoutException):
+        print("***** TIMEOUT ERROR when connecting to "+ip+" *****") 
+        return
+    except :
+        print("***** UNKONWN ERROR when connecting to "+ip+" *****")
+        return
     # use ntc_templates to parse the output into json
     parsed_result = parse_output(
             platform = dev_type,
